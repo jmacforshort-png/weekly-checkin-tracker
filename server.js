726f0ba3a76abc83ac2b83a06496de23477c899b
jmacforshort-png <jmacforshort@gmail.com>
2026-02-19 10:59:11 -0800

    UI polish

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-require("dotenv").config();
+require("dotenv").config(); 
 const express = require("express");
-const { google } = require("googleapis");
+const { google } = require("googleapis"); 
 
 const app = express();
 const port = process.env.PORT || 3000;
